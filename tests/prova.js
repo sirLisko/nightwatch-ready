1348b0d15b79e4cdb8c7d6d1a24d507ff3167362
Luca Lischetti <luca@sirlisko.com>
2016-06-06 18:30:12 +0100

    moved to ES2015

--- tests/prova.js
+++ tests/prova.js
@@ -1,23 +1,19 @@
-'use strict';
-
 module.exports = {
 
-	'follow on twitter': function(browser){
-		browser
-			.url('http://www.nightwatchjs.org')
-			.waitForElementVisible('body', 3000, 'page loaded')
-			.waitForElementPresent('.twitter-share-button', 3000)
-			.click('.twitter-share-button').pause(3000)
+	'follow on twitter': browser => browser
+		.url('http://www.nightwatchjs.org')
+		.waitForElementVisible('body', 3000, 'page loaded')
+		.waitForElementPresent('.twitter-share-button', 3000)
+		.click('.twitter-share-button').pause(3000)
 
-			.windowHandles(function(result) {
-				browser.assert.equal(result.value.length, 2, 'there is a second window.');
-				var handle = result.value[1];
-				browser.switchWindow(handle);
-			})
+		.windowHandles(result => {
+			browser.assert.equal(result.value.length, 2, 'there is a second window.');
+			var handle = result.value[1];
+			browser.switchWindow(handle);
+		})
 
-			.assert.urlContains('twitter')
-			.closeWindow()
-			.end();
-	}
+		.assert.urlContains('twitter')
+		.closeWindow()
+		.end();
 
 };
