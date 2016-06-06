f4b55d15d284e444d73dbf302569a46657d349f7
Luca Lischetti <luca@sirlisko.com>
2016-06-06 18:34:33 +0100

    moved to standard code style

--- tests/prova.js
+++ tests/prova.js
@@ -1,19 +1,19 @@
 module.exports = {
 
-	'follow on twitter': browser => browser
+  'follow on twitter': browser => browser
 		.url('http://www.nightwatchjs.org')
 		.waitForElementVisible('body', 3000, 'page loaded')
 		.waitForElementPresent('.twitter-share-button', 3000)
 		.click('.twitter-share-button').pause(3000)
 
-		.windowHandles(result => {
-			browser.assert.equal(result.value.length, 2, 'there is a second window.');
-			var handle = result.value[1];
-			browser.switchWindow(handle);
-		})
+    .windowHandles(result => {
+      browser.assert.equal(result.value.length, 2, 'there is a second window.')
+      var handle = result.value[1]
+      browser.switchWindow(handle)
+    })
 
 		.assert.urlContains('twitter')
 		.closeWindow()
-		.end();
+		.end()
 
-};
+}
